c21a769a3c41e107fc3181a170114c6079cb79e1
Carman Ngai <111025915+c-ngai@users.noreply.github.com>
2025-12-20 12:04:09 -0500

    Updated resume database for 2025-2026

--- components/AttendanceFormCard.jsx
+++ components/AttendanceFormCard.jsx
@@ -107,7 +107,7 @@ const AttendanceFormCard = (props) => {
           <Form.Text className="field-error">
               {failureMessage}
           </Form.Text>
-          <Form.Label className="resume-db"><a href="https://drive.google.com/drive/folders/1Nc1kvVmTYGP5yRZGou0BpX0qPjV7CxIBoRL3D9QZAZxQZjAFNi6-pt366JwkmEHpa0WrKTqy?usp=drive_link" target="_blank" rel="noopener noreferrer">Click Here To Add Your Resume To Our Database</a></Form.Label><br/>
+          <Form.Label className="resume-db"><a href="https://drive.google.com/drive/folders/13jPGhc4-XdhZeEAEvA5OleioY_T3R60av75oPoc1Y3jo0YBRMbqrxfjBxn1qG6Zx5t6JyePh?usp=sharing" target="_blank" rel="noopener noreferrer">Click Here To Add Your Resume To Our Database</a></Form.Label><br/>
           <Button variant="primary" type="submit">
             Submit
           </Button>
